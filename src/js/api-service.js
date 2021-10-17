0bd308d3c08973594742f229844d6a03c3f538cb
Natalia Odinets <natashaodinec57@gmail.com>
2021-10-17 12:53:40 +0300

    Добавляет скрипты

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,10 +1,71 @@
+import previewCountryTpl from '../templates/countryTemplate.hbs'
+import listFindCountry from '../templates/listCountry.hbs'
+
+import refs from './refs';
+
+import { error, notice } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
 const BASE_URL = 'https://restcountries.com/v2/name';
 
-export default {
-fetchCountries (searchQuery) {
-    return fetch(`${BASE_URL}/${searchQuery}`)
-    .then(response => response.json())
-}
+export default class NewsApiService {
+  constructor() {
+    this.searchQuery = '';
+//  this.input = ''
+  }
+    fetchCountries() {
+      const url = `${BASE_URL}/${this.searchQuery}`;
+    
+      return fetch(url)
+        .then(response => response.json())
+        .then(this.dataShow)
+        .catch(this.errorInfo);
+    
+    }
+      
+    get query() {
+    return this.searchQuery;
+    }
+
+    set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+    dataShow(countries) {
+     const { countriesMarkup } = refs;
+
+    if (countries.length > 10) {
+        notice({
+            title: 'Notification',
+            text: 'Too many matches found. Please enter a more specific query!',
+            delay: 3000,
+        });
+    }
+    else if (countries.status === 404) {
+        notice({
+            title: 'Notification',
+            text: 'Not Found. Please enter a more specific query!',
+        })
+    }
+    else if (countries.length > 1 && countries.length < 10) {
+        countriesMarkup.innerHTML = listFindCountry(countries);
+    }
+    else if (countries.length === 1) {
+        countriesMarkup.innerHTML = previewCountryTpl(...countries);
+    }
+    else if (this.query.length < 2 && this.query === " ")
+        return;
+
+    };
+
+    errorInfo() {
+
+    error({
+        text: 'Invalid entered value',
+        delay: 3000,
+    });
+    }
 } 
 
  
--- src/js/searchCountry.js
+++ src/js/searchCountry.js
@@ -1,61 +1,34 @@
 import debounce from 'lodash.debounce';
 
-
-import refs from './refs';
-import searchCountry from './api-service';
-
-import previewCountryTpl from '../templates/countryTemplate.hbs'
-import listFindCountry from '../templates/listCountry.hbs'
-
-
-import { error, notice } from '@pnotify/core';
+import { notice } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
+import refs from './refs';
+import NewsApiService from './api-service';
 
-const { search, countriesMarkup } = refs
+const { search, countriesMarkup } = refs;
 
-const searchInput = e => {
-    const searchQuery = e.target.value;
+const newsApiService = new NewsApiService();
+
+function searchInput(e) {
+    
+    newsApiService.query = e.target.value;
    
-    countriesMarkup.innerHTML = '';
-
-    searchCountry.fetchCountries(searchQuery)
-            .then(dataShow)
-            .catch(errorInfo);
-};
-
-function dataShow(countries) {
- 
-    if (countries.length > 10) {
-        notice({
-            title: 'Notification',
-            text: 'Too many matches found. Please enter a more specific query!',
+    if (newsApiService.query === ' ') {
+        return  notice({
+            text: 'enter your query',
             delay: 3000,
-        });
+        }) ;
     }
-    else if (countries.status === 404) {
-        console.log(countries.status);
-        notice({
-            title: 'Notification',
-            text: 'Not Found. Please enter a more specific query!',
-        })
-    }
-    else if (countries.length > 1 && countries.length < 10) {
-        countriesMarkup.innerHTML = listFindCountry(countries);
-    }
-    else if (countries.length === 1) {
-        countriesMarkup.innerHTML = previewCountryTpl(...countries);
-    }
-    else if (searchQuery.length < 2 && searchQuery === " ")
-        return;
+   
+    newsApiService.fetchCountries();
+      clearContainer() 
 
 };
 
-function errorInfo  () {
+function clearContainer() {
+  countriesMarkup.innerHTML = '';
+}
 
-        error({
-            text: 'Invalid entered value',
-        });
-    }
 search.addEventListener('input', debounce(searchInput, 500));
